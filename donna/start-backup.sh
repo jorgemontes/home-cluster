b5dbeb2690aca8c82e5dcf055a8311d8741e0df8
Jorge Montes <jorge.a.montes@pm.me>
2022-06-06 14:07:46 -0500

    created script to start all backups

---
+++ donna/start-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker start rclone-readarr
+docker start rclone-radarr
+docker start rclone-sonarr
+docker start rclone-tautulli
+docker start rclone-bazarr
+docker start rclone-prowlarr
+
