970c6fa7c2d0c0039604206466995f3b06740083
Jorge Montes <jorge.a.montes@pm.me>
2023-05-22 18:31:52 -0500

    Update wireguard-ping.sh

--- nico/wireguard-ping.sh
+++ nico/wireguard-ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$WG_STATUS = sudo systemctl status wg-quick@wg0 | grep "active (exited)"
+$WG_STATUS = `sudo systemctl status wg-quick@wg0 | grep "active (exited)"`
 if [ -n "$WG_STATUS" ]; then
     wget http://yiya:3001/api/push/s2sv2AZET2?status=up
 else
