b95fdb4b1e591d82ecc9785014faaae2a6a3b1e9
Jorge Montes <jorge.a.montes@pm.me>
2023-07-23 18:39:52 -0500

    Update start-backup.sh

--- donna/start-backup.sh
+++ donna/start-backup.sh
@@ -3,7 +3,6 @@ echo `date`
 docker start rclone-readarr
 docker start rclone-radarr
 docker start rclone-sonarr
-docker start rclone-tautulli
 docker start rclone-bazarr
 docker start rclone-prowlarr
 docker start rclone-transmission
