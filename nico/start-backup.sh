4c178585f889506d012591b4987b39aa2d59a323
Jorge Montes <jorge.a.montes@pm.me>
2023-08-24 20:29:05 -0500

    Update start-backup.sh

--- nico/start-backup.sh
+++ nico/start-backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo `date`
 docker start rclone-heimdall
+sleep 300
 docker start rclone-vikunja-db
+sleep 300
 docker start rclone-vikunja-api
+sleep 300
 docker start rclone-pihole
+sleep 300
 docker start rclone-tautulli
+sleep 300
