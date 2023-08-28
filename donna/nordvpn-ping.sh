45c6746f2f427516a83707b34c9d60c1576b3a0f
Jorge Montes <jorge.a.montes@pm.me>
2023-08-27 19:50:15 -0500

    Update nordvpn-ping.sh

--- donna/nordvpn-ping.sh
+++ donna/nordvpn-ping.sh
@@ -2,7 +2,7 @@
 NORDVPN_STATUS=$(nordvpn status | grep -E 'Connected|United States' | wc -l)
 echo "$NORDVPN_STATUS"
 if [ "$NORDVPN_STATUS" = '2' ]; then
-    wget -O /dev/null "http://yiya:3001/api/push/hW6s2LD1db?status=up&msg=OK&ping="
+    wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=up&msg=OK&ping="
 else
-    wget -O /dev/null "http://yiya:3001/api/push/hW6s2LD1db?status=down&msg=OK&ping="
+    wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=down&msg=OK&ping="
 fi
