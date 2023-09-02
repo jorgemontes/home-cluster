f0aa8bcb17b15b487651409b66c04d49a6b1b556
Jorge Montes <jorge.a.montes@pm.me>
2023-09-01 19:45:19 -0500

    Update nordvpn-ping.sh

--- donna/nordvpn-ping.sh
+++ donna/nordvpn-ping.sh
@@ -4,5 +4,5 @@ echo "$NORDVPN_STATUS"
 if [ "$NORDVPN_STATUS" = '2' ]; then
     wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=up&msg=OK&ping="
 else
-    wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=down&msg=OK&ping="
+    wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=down&msg=DOWN&ping="
 fi
