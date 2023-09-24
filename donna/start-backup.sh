0154ac1d272b9aa96b87b787600e93c7c6f27131
Jorge Montes <jorge.a.montes@pm.me>
2023-09-24 10:17:28 -0500

    Update start-backup.sh

--- donna/start-backup.sh
+++ donna/start-backup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-echo `date`
-docker start rclone-radarr
-docker start rclone-sonarr
-docker start rclone-bazarr
-docker start rclone-prowlarr
-docker start rclone-transmission
-
+echo `date` > exe-log.log
+docker start rclone-radarr >> exe-log.log
+sleep 300
+docker start rclone-sonarr >> exe-log.log
+sleep 300
+docker start rclone-bazarr >> exe-log.log
+sleep 300
+docker start rclone-prowlarr >> exe-log.log
+sleep 300
+docker start rclone-transmission >> exe-log.log
+sleep 300
+echo `date` >> exe-log.log
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d '{"chat_id": "123456789", "text": "This is a test from curl", "disable_notification": true}' \
+     https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
