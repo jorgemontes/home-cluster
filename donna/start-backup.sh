e63f0f4d5413d69b03eaa864260287ca83c4520f
Jorge Montes <jorge.a.montes@pm.me>
2023-09-30 15:54:07 -0500

    Update start-backup.sh

--- donna/start-backup.sh
+++ donna/start-backup.sh
@@ -2,7 +2,7 @@
 TELEGRAM_BOT_TOKEN=
 CHAT_ID=
 TEXT='\U+1F4BE Backup de Donna finalizado'
-JSON='{"chat_id": $CHAT_ID, "text": $TEXT}'
+JSON='{"chat_id": $chat_id, "text": $text}'
 echo `date` > exe-log.log
 docker start rclone-radarr >> exe-log.log
 sleep 300
@@ -16,6 +16,8 @@ docker start rclone-transmission >> exe-log.log
 sleep 300
 echo `date` >> exe-log.log
 CONTENT=`cat exe-log.log`
+TEXT=`$TEXT $CONTENT`
+JSON=`jq -n --arg chat_id "$CHAT_ID" --arg text "$TEXT" $JSON`
 curl -X POST \
      -H 'Content-Type: application/json' \
      -d $JSON \
