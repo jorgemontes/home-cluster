3d349842e69d430a24836f982cb94d4c4441adef
Jorge Montes <jorge.a.montes@pm.me>
2023-10-02 21:22:49 -0500

    fixed backup

--- nico/start-backup.sh
+++ nico/start-backup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-echo `date`
-docker start rclone-heimdall
-sleep 300
-docker start rclone-vikunja-db
-sleep 300
-docker start rclone-vikunja-api
-sleep 300
-docker start rclone-pihole
-sleep 300
-docker start rclone-tautulli
-sleep 300
-docker start rclone-pivpn
+
+source /home/pi/git/home-cluster/nico/backup-config.cfg
+TEXT="Backup de $HOSTNAME finalizado"
+
+echo `date` > exe-log.log
+# Iterar por la lista de contenedores y realizar acciones en cada uno
+for container in "${CONTAINERS[@]}"; do
+  echo "Iniciando el contenedor $container" >> exe-log.log
+  docker start "$container" >> exe-log.log
+  sleep 300
+done
+echo `date` >> exe-log.log
+
+CONTENT=`cat exe-log.log`
+TEXT="$TEXT "
+JSON="{\"chat_id\": \"$CHAT_ID\", \"text\": \"$TEXT\"}"
+echo $JSON > telegram.json 
+
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d @telegram.json \
+     https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
--- yiya/start-backup.sh
+++ yiya/start-backup.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-echo `date`
-docker start rclone-uptime-kuma
-sleep 300
-docker start rclone-pihole
 
+source /home/pi/git/home-cluster/yiya/backup-config.cfg
+TEXT="Backup de $HOSTNAME finalizado"
+
+echo `date` > exe-log.log
+# Iterar por la lista de contenedores y realizar acciones en cada uno
+for container in "${CONTAINERS[@]}"; do
+  echo "Iniciando el contenedor $container" >> exe-log.log
+  docker start "$container" >> exe-log.log
+  sleep 300
+done
+echo `date` >> exe-log.log
+
+CONTENT=`cat exe-log.log`
+TEXT="$TEXT "
+JSON="{\"chat_id\": \"$CHAT_ID\", \"text\": \"$TEXT\"}"
+echo $JSON > telegram.json 
+
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d @telegram.json \
+     https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
