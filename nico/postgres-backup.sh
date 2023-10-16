68d652c10d544bbd9b47e0a3565fb33109a148ba
Jorge Montes <jorge.a.montes@pm.me>
2023-10-16 15:03:33 -0500

    added backup script

--- nico/postgres-backup.sh
+++ nico/postgres-backup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker exec vikunja-db echo "db:5432:vikunja:secret" > ~/.pgpass
-docker exec vikunja-db chmod 0600 ~/.pgpass
-docker exec vikunja-db pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).tar.gz
+docker exec -it vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).tar.gz'
 
