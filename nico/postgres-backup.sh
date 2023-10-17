df04f5203f3475da48da910316ae70a8456c636b
Jorge Montes <jorge.a.montes@pm.me>
2023-10-16 19:02:06 -0500

    Update postgres-backup.sh

--- nico/postgres-backup.sh
+++ nico/postgres-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -it vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).tar.gz'
+docker exec -it vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
 
