5b4f1972a43026d65ed25aff4627b87a4627897f
Jorge Montes <jorge.a.montes@pm.me>
2023-11-13 12:15:04 -0500

    Update start-backup.sh

--- yiya/start-backup.sh
+++ yiya/start-backup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 source /home/pi/git/home-cluster/yiya/backup-config.cfg
-TEXT="Backup de **$HOSTNAME** finalizado"
+TEXT="Backup de $HOSTNAME finalizado"
 
 echo `date` > exe-log.log
 # Iterar por la lista de contenedores y realizar acciones en cada uno
 for container in "${CONTAINERS[@]}"; do
   echo "Iniciando el contenedor $container" >> exe-log.log
   docker start "$container" >> exe-log.log
-  sleep 300
+  sleep 30
 done
 echo `date` >> exe-log.log
 
 CONTENT=`cat exe-log.log`
-TEXT="$TEXT "
-JSON="{\"chat_id\": \"$CHAT_ID\", \"text\": \"$TEXT\"}"
+LENGTH=`wc -c < exe-log.log`
+HOSTNAME_LENGTH=`echo $HOSTNAME | wc -c `
+TEXT="$TEXT $CONTENT   "
+JSON="{\"chat_id\": \"$CHAT_ID\", \"text\": \"$TEXT\", \"entities\":[{\"type\":\"bold\",\"offset\":10,\"length\":$HOSTNAME_LENGTH},{\"type\":\"pre\",\"offset\":27,\"length\":$LENGTH}]}"
 echo $JSON > telegram.json 
 
 curl -X POST \
