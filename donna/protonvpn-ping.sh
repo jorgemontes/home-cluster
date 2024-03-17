cf699dec3012edb5de87444cb4667605b5333a61
Jorge Montes <jorge.a.montes@pm.me>
2024-03-17 18:00:44 -0500

    fixed var

--- donna/protonvpn-ping.sh
+++ donna/protonvpn-ping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROTONVPN_STATUS=$(sudo wg show | grep 'latest handshake' | wc -l)
 echo "$PROTONVPN_STATUS"
-if [ "$NORDVPN_STATUS" = '1' ]; then
+if [ "$PROTONVPN_STATUS" = '1' ]; then
     wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=up&msg=OK&ping="
 else
     wget -O /dev/null "http://192.168.66.190:3001/api/push/hW6s2LD1db?status=down&msg=DOWN&ping="
