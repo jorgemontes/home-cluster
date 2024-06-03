a00b4fc67d03a04bd3dd94a97fca327323d75011
Jorge Montes <jorge.a.montes@pm.me>
2024-06-02 19:26:34 -0500

    Update external-apps-backup.sh

--- nico/external-apps-backup.sh
+++ nico/external-apps-backup.sh
@@ -3,7 +3,5 @@ date
 
 /usr/local/bin/pihole -a -t /home/pi/pihole-backup/pi-hole-nico-teleporter_$(date +%Y-%m-%d).tar.gz
 
-/usr/local/bin/pivpn -bk
-
 /usr/bin/docker exec -it vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
 
