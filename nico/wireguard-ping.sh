0a4e0637b0c6c4d45e8d350f79d3a16956f02912
Jorge Montes <jorge.a.montes@pm.me>
2024-06-02 19:27:27 -0500

    Delete nico/wireguard-ping.sh

--- nico/wireguard-ping.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-WG_STATUS=$(sudo systemctl status wg-quick@wg0 | grep "active (exited)")
-echo "$WG_STATUS"
-if [ -n "$WG_STATUS" ]; then
-    wget -O /dev/null "http://yiya:3001/api/push/s2sv2AZET2?status=up"
-else
-    wget -O /dev/null "http://yiya:3001/api/push/s2sv2AZET2?status=down"
-fi
-
