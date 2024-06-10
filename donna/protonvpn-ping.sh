0a8a0a3de54c48e95802ac0d00ded97cccf50d53
Jorge Montes <jorge.a.montes@pm.me>
2024-06-09 20:59:36 -0500

    Update protonvpn-ping.sh

--- donna/protonvpn-ping.sh
+++ donna/protonvpn-ping.sh
@@ -2,7 +2,7 @@
 PROTONVPN_STATUS=$(sudo wg show | grep 'latest handshake' | wc -l)
 echo "$PROTONVPN_STATUS"
 if [ "$PROTONVPN_STATUS" = '1' ]; then
-    wget -O /dev/null "http://yiya:3002/api/push/CSWhDjYCYw?status=up&msg=OK&ping="
+    wget -O /dev/null "http://yiya:3001/api/push/CSWhDjYCYw?status=up&msg=OK&ping="
 else
-    wget -O /dev/null "http://yiya:3002/api/push/CSWhDjYCYw?status=down&msg=DOWN&ping="
+    wget -O /dev/null "http://yiya:3001/api/push/CSWhDjYCYw?status=down&msg=DOWN&ping="
 fi
