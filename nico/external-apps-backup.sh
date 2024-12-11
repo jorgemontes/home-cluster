e5ceeea1e196378cb22eb37ad291478cb294a8d4
Jorge Montes <jorge.a.montes@pm.me>
2024-12-11 13:59:36 -0500

    Update external-apps-backup.sh

--- nico/external-apps-backup.sh
+++ nico/external-apps-backup.sh
@@ -4,9 +4,9 @@ date
 
 /usr/local/bin/pihole -a -t /home/pi/pihole-backup/pi-hole-nico-teleporter_$(date +%Y-%m-%d).tar.gz
 
-/usr/bin/docker exec vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
+/usr/bin/docker exec vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h vikunja-db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
 echo "Finished with file /backup/vikunja_db-$(date +%Y-%m-%d).gz"
 
-/usr/bin/docker exec gnucash-db sh -c 'PGPASSWORD="secret" pg_dump -U gnucash -h db | gzip > /backup/gnucash_db-$(date +%Y-%m-%d).gz'
+/usr/bin/docker exec gnucash-db sh -c 'PGPASSWORD="secret" pg_dump -U gnucash -h gnucash-db | gzip > /backup/gnucash_db-$(date +%Y-%m-%d).gz'
 echo "Finished with file /backup/gnucash_db-$(date +%Y-%m-%d).gz"
 
