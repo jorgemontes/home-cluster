150d6c9665e26e1b52c4a162f7f1acfa00752c30
Jorge Montes <jorge.a.montes@pm.me>
2024-12-11 10:24:22 -0500

    Update external-apps-backup.sh

--- nico/external-apps-backup.sh
+++ nico/external-apps-backup.sh
@@ -7,7 +7,6 @@ date
 /usr/bin/docker exec vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
 echo "Finished with file /backup/vikunja_db-$(date +%Y-%m-%d).gz"
 
-
 /usr/bin/docker exec gnucash-db sh -c 'PGPASSWORD="secret" pg_dump -U gnucash -h db | gzip > /backup/gnucash_db-$(date +%Y-%m-%d).gz'
 echo "Finished with file /backup/gnucash_db-$(date +%Y-%m-%d).gz"
 
