1d3bc4f90fdcb2f81b7fa76d1a5bd106d21351e0
Jorge Montes <jorge.a.montes@pm.me>
2025-06-03 20:31:20 -0500

    Update external-apps-backup.sh

--- nico/external-apps-backup.sh
+++ nico/external-apps-backup.sh
@@ -2,7 +2,8 @@
 whoami
 date
 
-/usr/bin/pihole-FTL --teleporter /home/pi/pihole-backup/pi-hole-nico-teleporter_$(date +%Y-%m-%d).tar.gz
+cd /home/pi/pihole-backup/
+sudo /usr/bin/pihole-FTL --teleporter
 
 /usr/bin/docker exec vikunja-db sh -c 'PGPASSWORD="secret" pg_dump -U vikunja -h vikunja-db | gzip > /backup/vikunja_db-$(date +%Y-%m-%d).gz'
 echo "Finished with file /backup/vikunja_db-$(date +%Y-%m-%d).gz"
